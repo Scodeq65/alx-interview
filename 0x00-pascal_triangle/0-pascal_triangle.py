611813f637021795b485283092867ec0b9ab9be9
scodeq65 <sodiqagbaraojo@gmail.com>
2024-10-07 02:48:20 +0100

    modified

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,34 +1,40 @@
-#!/usr/bin/phthon3
+#!/usr/bin/python3
 """
-Module that contains the Pascal_triangle function.
-This func generate pascal's triangle uo to a given nos of rows.
+Module that contains the pascal_triangle function.
+This function generates Pascal's triangle up to a given number of rows.
 """
 
 
 def pascal_triangle(n):
-  """
-  Returns a lists of int representing pascal's triangle
+    """
+    Returns a list of lists of integers
+    representing Pascal's Triangle up to n rows.
 
-  Args:
-    n (int): The nos of rows to generate in Pascal's Triangle.
+    Args:
+        n (int): The number of rows to generate in Pascal's Triangle.
 
-  Returns:
-    list: a list of lists, where each inner list represents a row of a Pascal's triangle
-  """
-  if n <= 0:
-    return []
+    Returns:
+        list: A list of lists, where each inner list represents
+        a row of Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
 
-  """The first role of a pascal triangle is always [1]"""
-  triangle = [[1]]
+    """ The first row of Pascal's Triangle is always [1]
+    """
+    triangle = [[1]]
 
-  for i in range(1, n):
-    """the first element of each row is always 1"""
-    row =[1]
-    for j in range(1, i):
-      """Each element is the sum of the two elements directly above it."""
-      row.append(triangle[i - 1][j -1] + triangle[i -1][j])
-    """The last element of each row is always 1"""
-    row.append(1)
-    triangle.append(row)
+    for i in range(1, n):
+        """ The first element of each row is always 1
+        """
+        row = [1]
+        for j in range(1, i):
+            """ Each element is the sum of the two elements directly above it
+            """
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        """ The last element of each row is always 1
+        """
+        row.append(1)
+        triangle.append(row)
 
-  return triangle
+    return triangle
