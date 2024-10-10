06aba786ab1e0c8bc9bb59590ddcc5e536cc7987
scodeq65 <sodiqagbaraojo@gmail.com>
2024-10-10 12:54:35 +0100

    lockedboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+
+    Args:
+        boxes (list of lists): Each list contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, else False.
+    """
+    n = len(boxes)
+    opened_boxes = set([0])  # Start with the first box (0) unlocked
+    keys = set(boxes[0])  # Keys found in the first box
+
+    # Traverse boxes while there are keys to use
+    while keys:
+        key = keys.pop()  # Get a key
+        """ Valid key and box not yet opened"""
+        if key < n and key not in opened_boxes:
+            opened_boxes.add(key)  # Mark the box as opened
+            keys.update(boxes[key])  # Add the keys from this box to the set
+
+    # If all boxes have been opened, return True
+    return len(opened_boxes) == n
